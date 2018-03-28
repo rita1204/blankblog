84afa78a73498f5c08f223a92a00175cf7f11307
rita1204 <pittixichan@gmail.com>
2018-03-28 12:18:40 +0900

    users/show edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,39 @@
 class UsersController < ApplicationController
+  before_action :set_user, except: [:new,:create]
   def new
     @user = User.new
   end
 
   def create
-    debugger
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the blank blog #{@user.username}!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:email,:password)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
