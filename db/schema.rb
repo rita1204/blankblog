745e5c8171633f462a568b65ff1b598efd19a3a0
rita1204 <pittixichan@gmail.com>
2018-04-11 17:03:20 +0900

    change_db_to_mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,20 @@
 ActiveRecord::Schema.define(version: 20180330080844) do
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer  "user_id",     limit: 4
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "password_digest"
-    t.boolean  "admin",           default: false
+    t.string   "username",        limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.string   "password_digest", limit: 255
+    t.boolean  "admin",                       default: false
   end
 
 end
